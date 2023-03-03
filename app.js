05d7169c4140b3ba5890a2c73c98438eafec4b56
Andrew <bulbuliver@gmail.com>
2023-03-03 19:26:12 +0300

    slider done

--- app.js
+++ app.js
@@ -8,3 +8,27 @@ function toggleClass() {
 }
 
 toggleClass();
+
+// Swiper
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
